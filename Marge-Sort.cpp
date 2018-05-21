b1e1d179201d6c394019c341c8e4f74d8bbe55d5
MuhamadIqbal1030 <38530730+MuhamadIqbal1030@users.noreply.github.com>
2018-05-22 05:06:11 +0700

    Update Marge-Sort.cpp

--- Marge-Sort.cpp
+++ Marge-Sort.cpp
@@ -1,77 +1,80 @@
 #include <iostream>
-Using namespace std;
+#include <conio.h>
+using namespace std;
 
-void Merge(int* A, int kiri,int tengah, int kanan){
-int B[kiri+kanan];
-int i,bagian1,bagian2;
-bagian1=kiri;
-bagian2=tengah+1;
-i=kiri;
-while (bagian1<=tengah && bagian2 <= kanan){
-if(A[bagian1] <= A[bagian2]){
-B[i]=A[bagian1];
-bagian1++;
-}
-else{
-B[i]=A[bagian2];
-Bagian2++;
-}
-i++;
-}
-    while ( bagian1 <= tengah ){
-        B[i] = A[bagian1];
-        bagian1++;
-        i++;
-        }
+void M(int *a, int rendah, int tinggi, int tengah){
+	int i, j, k, temp[tinggi-rendah+1];
+	i = rendah;
+	k = 0;
+	j = tengah + 1;
+    
+	while (i <= tengah && j <= tinggi)
+	{
+		if (a[i] < a[j])
+		{
+			temp[k] = a[i];
+			k++;
+			i++;
+		}
+		else
+		{
+			temp[k] = a[j];
+			k++;
+			j++;
+		}
+	}
+    
+	while (i <= tengah)
+	{
+		temp[k] = a[i];
+		k++;
+		i++;
+	}
 
-    while ( bagian2 <= kanan ){
-        B[i] = A[bagian2];
-        bagian2++;
-        i++;
-        }
+	while (j <= tinggi)
+	{
+		temp[k] = a[j];
+		k++;
+		j++;
+	}
 
-for (int i=kiri;i<= kanan;i++){
-A[i]=B[i];
-}
-
-}
-
-void MergeSort (int* A, int i, int j){
-int k;
-if (i<j){
-k= ((i+j)/2);
-MergeSort(A, i, k);
-MergeSort(A, k+1, j);
-Merge(A, i, k, j);
-}
+    for (i = rendah; i <= tinggi; i++)
+	{
+		a[i] = temp[i-rendah];
+	}
 }
 
-int main(int argc, char *argv[])
+void S(int *a, int rendah, int tinggi)
 {
-int n;
-int i;
-int j;
-cout<<"Rifky,Iqbal,Azhari"; 
-cout<<"Banyak data :";
-cin>>n;
-i=1;
-j=n;
-int A[n];
-for (int x=1;x<=n;x++){
-cout<<"Masukan data ke-"<<x<<" : ";
-cin>>A[x];
+	int tengah;
+	if (rendah < tinggi)
+	{
+		tengah=(rendah+tinggi)/2;
+
+		S(a, rendah, tengah);
+		S(a, tengah+1, tinggi);
+
+		M(a,rendah, tinggi, tengah);
+	}
 }
-cout<<"Data Sebelum diurutkan "<<endl; 
-for (int x=1;x<=j;x++){
-cout<<A[x]<<" ";
-    }
-    cout<<endl;
-MergeSort(A,i,j);
-cout<<"Data Setelah diurutkan "<<endl; 
-for (int x=1;x<=j;x++){
-cout<<A[x]<<" ";
-    }
-    cout<<endl;
-system("pause");
-return 0;
+int main()
+{
+	int n, i;
+	cout<<"\nBanyaknya nilai yang ingin diinputkan: ";
+	cin>>n;
+
+	int A[n];
+	for(i = 0; i < n; i++)
+	{
+		cout<<"Masukan nilai ke "<<i+1<<": ";
+		cin>>A[i];
+	}
+
+	S(A, 0, n-1);
+
+	cout<<"\nUrutan Nilai : ";
+	for (i = 0; i < n; i++)
+        cout<<" "<<A[i];
+
+	getch();
 }
